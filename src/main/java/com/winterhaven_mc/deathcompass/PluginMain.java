bdd6b6bd61e5d50b82fe390a8eb79c00c901533a
Tim Savage <tim.savage@gmail.com>
2022-01-02 04:54:42 -0600

    update to latest versions of libraries

--- src/main/java/com/winterhaven_mc/deathcompass/PluginMain.java
+++ src/main/java/com/winterhaven_mc/deathcompass/PluginMain.java
@@ -7,10 +7,10 @@ import com.winterhaven_mc.deathcompass.messages.Macro;
 import com.winterhaven_mc.deathcompass.messages.MessageId;
 import com.winterhaven_mc.deathcompass.storage.DataStore;
 
-import com.winterhaven_mc.util.WorldManager;
-import com.winterhaven_mc.util.SoundConfiguration;
-import com.winterhaven_mc.util.YamlSoundConfiguration;
 import com.winterhavenmc.util.messagebuilder.MessageBuilder;
+import com.winterhavenmc.util.soundconfig.SoundConfiguration;
+import com.winterhavenmc.util.soundconfig.YamlSoundConfiguration;
+import com.winterhavenmc.util.worldmanager.WorldManager;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
